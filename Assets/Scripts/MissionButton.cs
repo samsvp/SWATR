6c4bccaadcf073af86805e88d0dcc88a057d66fa
Samuel Simplicio Valentim <samuelsimplicio5@gmail.com>
2020-08-06 17:15:37 -0300

    Wrote the mission briefings.

---
+++ Assets/Scripts/MissionButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MissionButton : MonoBehaviour
+{
+
+    public void StartMission()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void PointerEnter()
+    {
+        transform.localScale = new Vector3(1.3f, 1.3f);
+    }
+
+    public void PointerExit()
+    {
+        transform.localScale = Vector3.one;
+    }
+}
