0a4231a5fc3a5b32ef3b0f966b7c02b10de08aba
Samuel Simplicio Valentim <samuelsimplicio5@gmail.com>
2020-08-12 02:31:09 -0300

    Started working on a vision system

---
+++ Assets/Scripts/Vision.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vision : MonoBehaviour
+{
+
+    private int index;
+    private Transform nextChild;
+    private Transform parent;
+    private bool lastSibling = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        index = transform.GetSiblingIndex();
+        parent = transform.parent;
+        if (index < parent.childCount - 1) nextChild = parent.GetChild(index + 1);
+        else lastSibling = true;
+    }
+    
+    
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (!lastSibling) nextChild.gameObject.SetActive(false);
+    }
+
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (!lastSibling) nextChild.gameObject.SetActive(true);
+    }
+
+
+    public void OnEnable()
+    {
+        if (!lastSibling) nextChild.gameObject.SetActive(true);
+    }
+
+
+    public void OnDisable()
+    {
+        if (!lastSibling) nextChild.gameObject.SetActive(false);
+    }
+
+}
